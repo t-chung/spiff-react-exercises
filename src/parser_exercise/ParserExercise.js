4ece6f79cc9aa6ae210ea378a0befb63f12c9cd7
t-chung <taratchung@gmail.com>
2021-07-31 16:36:36 -0700

    Implement letter count and displaying results

--- src/parser_exercise/ParserExercise.js
+++ src/parser_exercise/ParserExercise.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import Exercise from "../exercise/Exercise";
 import "./ParserExercise.css";
 
@@ -20,12 +20,39 @@ export default ParserExercise;
 
 const Solution = () => {
   const [inputString, setInputString] = useState("");
+  const [output, setOutput] = useState({});
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const letterCount = {};
+    // initialize leterCount with all letters of the alphabet
+    alphabet.forEach((letter) => {
+      letterCount[letter] = 0;
+    });
+    // traverse the inputString and count each alphabetical letter
+    [...inputString].forEach((char) => {
+      if (letterCount.hasOwnProperty(char)) {
+        letterCount[char] = letterCount[char] + 1;
+      }
+    });
+    setOutput(letterCount);
   };
-  const resetClick = () => {
 
+  const displayOutput = useMemo(() => {
+    // create a list from output
+    return (
+      <div>
+        <ul>
+          {Object.keys(output).map((char) => {
+            return <li key={char}><b>{char}</b> {output[char]}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }, [output]);
+  const resetClick = () => {
+    setInputString("");
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -43,6 +70,7 @@ const Solution = () => {
       <button className="parserButton resetButton" onClick={resetClick}>
         RESET
 			</button>
+      {displayOutput}
     </form>
   );
 };
